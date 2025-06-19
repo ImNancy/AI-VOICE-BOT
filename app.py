06357c6e3bb02cf935ec86302eda45bf9d4dabe3
Nancy <137004093+ImNancy@users.noreply.github.com>
2025-06-19 21:30:59 +0530

    app.py

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
-from app import app
+#from app import app
 
+#if __name__ == '__main__':
+    #app.run(host='0.0.0.0', port=5000, debug=True)
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))  # <-- This allows Render to assign the right port
+    app.run(host='0.0.0.0', port=port, debug=True)
+
