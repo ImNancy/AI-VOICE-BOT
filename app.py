ec99f96f30975627863f5a9bb99bd0d63b40eb38
Nancy <137004093+ImNancy@users.noreply.github.com>
2025-06-20 00:18:15 +0530

    app.py

--- app.py
+++ app.py
@@ -5,4 +5,4 @@ application = app
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("app:app", host="0.0.0.0", port= 5000, reload=True)
