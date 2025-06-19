6d8b4a63e752c0052078bff9ad771d56924c172e
Nancy <137004093+ImNancy@users.noreply.github.com>
2025-06-19 23:41:44 +0530

    +app.py

--- app.py
+++ app.py
@@ -1,13 +1,4 @@
-#from app import app
+ from app import app
 
-#if __name__ == '__main__':
-    #app.run(host='0.0.0.0', port=5000, debug=True)
-from app import app
-
-# For gunicorn compatibility
-application = app
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=5000)
-
+ if __name__ == '__main__':
+     app.run(host='0.0.0.0', port=5000, debug=True)
