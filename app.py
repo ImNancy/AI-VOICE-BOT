275a3ed6294ce6f3c074d813d23156d3d689b814
Nancy <137004093+ImNancy@users.noreply.github.com>
2025-06-20 00:41:08 +0530

    Delete app.py

--- app.py
+++
@@ -1,8 +0,0 @@
-from app import app  # assuming your FastAPI app is in app.py
-
-# For gunicorn or uvicorn
-application = app
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run("app:app", host="0.0.0.0", port= 5000, reload=True)
